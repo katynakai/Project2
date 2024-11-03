fd3488d07ba3ba3cb715a3adc3443560e2365c54
katynakai <katynakai@berkeley.edu>
2024-11-02 18:55:27 -0700

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,18 @@
+const navbar = () => {
+    return <div>
+        <div id="header">   
+            <div class="logo">
+                <a href="index.html"><img src=".png"></img></a>
+            </div>
+            <div class="pages">
+                <a href="Project1.html">Work</a>
+                <a href="Project1_contact.html">Play</a>
+                <a href="Project1_contact.html">Resume</a>
+                <a href="Project1_contact.html">About</a>
+                <a href="Project1_contact.html">Contact</a>
+            </div>
+        </div>
+    </div>
+};
+
+export default navbar;
